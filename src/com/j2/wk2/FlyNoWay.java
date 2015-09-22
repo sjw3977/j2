30df42b436bfdff222173aba7b19d720b51c6423
unknown <siw3977@naver.com>
2015-09-22 14:05:45 +0900

    smu405

--- src/com/j2/wk2/FlyNoWay.java
+++ src/com/j2/wk2/FlyNoWay.java
@@ -2,6 +2,6 @@ package com.j2.wk2;
 
 public class FlyNoWay implements FlyBehavior {
   public void fly() {
-    System.out.println("I'm not flying!");
+    System.out.println("I can't flying!");
   }
 }
