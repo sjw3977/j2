252bb378bc5f9c943a82089d2a843f188d85bcd3
sjw3977 <sjw3977@naver.com>
2015-11-17 20:36:33 +0900

    home

--- src/com/j2/templatemethod/barista/BeverageTestDrive.java
+++ src/com/j2/templatemethod/barista/BeverageTestDrive.java
@@ -4,8 +4,12 @@ public class BeverageTestDrive {
   public static void main(String[] args) {
     Coffee coffee = new Coffee();
     Tea tea = new Tea();
-    coffee.prepareRecipe();
+    
+    System.out.println("\nMaking tea...");
     tea.prepareRecipe();
+    
+    System.out.println("\nMaking coffee...");
+    coffee.prepareRecipe();
   }
 }
     
